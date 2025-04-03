909122da3ce81f8652294943f770ee324e0f2352
armmmartirosyan <106014381+armmmartirosyan@users.noreply.github.com>
2025-04-03 20:24:19 +0400

    api connection fix

--- src/constants/index.js
+++ src/constants/index.js
@@ -35,7 +35,7 @@ export const ACTIVE_BTN_OPACITY = 0.9;
 
 export const RADIUS_LARGE = 30;
 
-export const API_BASE_URL = 'http://127.0.0.1:5122/';
+export const API_BASE_URL = 'http://192.168.110.117:5122/';
 
 export const DEFAULT_CODE = {
   callingCode: ['374'],
